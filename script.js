7aea1c922b20844a2e4044b6cb945617caf1de2c
samAd0san <sohaibsamad.hala@gmail.com>
2024-02-20 20:22:30 +0530

    Creating a node server

---
+++ script.js
@@ -0,0 +1,23 @@
+// Import the 'http' module 
+const http = require('http');
+
+// Create an HTTP server instance using the 'createServer' method and passing a request handler 
+// function named 'handler'
+const server = http.createServer(handler);
+
+// Define the request handler function, which responds with the text 'NodeJs' to any incoming 
+// request
+function handler(req,res) {
+    res.write('NodeJs');
+    res.end();
+}
+
+// Specify the port number on which the server will listen for incoming requests
+const port = 3000;
+
+// Start the server to listen for incoming requests on the specified port
+server.listen(port,()=> {
+    console.log('The server is running');
+});
+
+// console.log('The server is running');
