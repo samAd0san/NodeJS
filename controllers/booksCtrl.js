87ab81bee8972b5e3cb275c5f60a322dba4d4eb6
samAd0san <sohaibsamad.hala@gmail.com>
2024-02-28 15:02:24 +0530

    getById

--- controllers/booksCtrl.js
+++ controllers/booksCtrl.js
@@ -1,18 +1,39 @@
-const books = (req,res) => {
-    const books = [{id:1,name:'Atomic Habits',price:499},{id:2,name:'xDman',price:349}];
-    // Serialiazation
-    res.json(books);
-}
+const bookDb = [{id:1,name:'Atomic Habits',price:499},{id:2,name:'OniiSan',price:349}];
+const authorDb = ['James Bond','Karl bond'];
 
-const authors = (req,res) => {
-    const authors = ['James Bond','Karl rock'];
-    res.json(authors);
-}
-
-module.exports = {
-    // we can declare like this
-    books : books,
-
-    // or like this
-    authors,
+// Creating a handler function for callback (fn for bookDb)
+const books = (req,res)=> {
+    res.json(bookDb);
 };
+
+// This logic is to get the specific id of bookDb when passed in the request url
+const getById = (req,res) => {
+    // This is for checking the path in which, what id is given
+    const id = parseInt(req.params.id);
+
+    const filterFn = (elem) =>{
+        // left side id - request id
+        // right side id - bookDb id (It travese through book object to check wheter it matches the req or not)
+        return elem.id === id;
+    };
+    const filteredBooks = bookDb.filter(filterFn);
+
+    // add '[0]' after filteredBooks 
+    if(filteredBooks){
+        res.status(200).json(filteredBooks);
+    }else{
+        res.status(404).send('Not found');
+    }
+};
+
+// Creating a handler function for callback (fn for authorsDb)
+const authors = (req,res)=> {
+    res.json(authorDb);
+}
+
+// exporting the modules so that other files can access it
+module.exports = {
+    books,
+    authors,
+    getById,
+}
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 // calling the custom module from the bookCtrl file
 const booksCtrl = require('../controllers/booksCtrl');
 
@@ -6,7 +6,9 @@ const booksCtrl = require('../controllers/booksCtrl');
 const router = express.Router();
 
 // giving the route/path by using router which is the predefined object of express module
-router.use('/books',booksCtrl.books);
-router.use('/authors',booksCtrl.authors);
+router.get('/books',booksCtrl.books);
+router.get('/authors',booksCtrl.authors);
+// added logic
+router.get('/books/:id',booksCtrl.getById);
 
 module.exports = router;
