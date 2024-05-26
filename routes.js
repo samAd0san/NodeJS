9c6d76f93031b437d6e00167f671bd667310f077
samAd0san <sohaibsamad.hala@gmail.com>
2024-05-26 10:52:31 +0530

    deploy on render

--- routes.js
+++ routes.js
@@ -19,27 +19,28 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Allow requests from http://localhost:5173
-app.use(cors({ origin: 'http://localhost:5173' }));
+// app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors());
 
 /* If the directory does not already exist, it first checks for its existence using fs.existsSync(). If the directory doesn't
 exist, it creates the directory using fs.mkdirSync(). This is typically done to ensure that a logs directory exists before
 logging any data to files within it. */
 
-const logsDir = path.join(__dirname,'logs');
+const logsDir = path.join(__dirname, 'logs');
 
-if(!fs.existsSync(logsDir)){
+if (!fs.existsSync(logsDir)) {
     fs.mkdirSync(logsDir);
 }
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`The port is running on ${port}`);
 })
 
-const fsStream = fs.createWriteStream(path.join(__dirname,'logs','app.log'),{flags : 'a'});
+const fsStream = fs.createWriteStream(path.join(__dirname, 'logs', 'app.log'), { flags: 'a' });
 // const fsStream = fs.createWriteStream(__dirname + "/logs/request.log", {flags : 'a'}); // flag : a -> appends all the logs to requset.log file
 
 // app.use(morgan('dev')); // This will display the logs on the shell
-app.use(morgan('combined', {stream : fsStream})); // This will add all the logs to the request.log file 
+app.use(morgan('combined', { stream: fsStream })); // This will add all the logs to the request.log file 
 
 // when creating the new resources using post i was getting null so resolve that
 app.use(express.json());
@@ -49,7 +50,6 @@ app.use(express.json());
 
 // This is to connect to the remote mongodb
 const conStr = process.env.dbConStr || 'mongodb+srv://admin:admin@samadscluster.a4s9jvf.mongodb.net/cgc-remote-db';
-// const conStr = 'mongodb+srv://admin:admin@samadscluster.a4s9jvf.mongodb.net/cgc-remote-db';
 mongoose.connect(conStr);
 
 console.log('db Connected');
@@ -58,14 +58,14 @@ app.use(express.static('uploads/'));
 
 // READ Operation in CRUD
 app.use(homeRoutes);
-app.use('/users',userRoutes);
+app.use('/users', userRoutes);
 
 // app.use(auth.basicAuth);
-// app.use(auth.tokenAuth);
+app.use(auth.tokenAuth);
 
 app.use(bookRoutes);
 app.use(productRoutes);
 
-app.use((req,res) => {
+app.use((req, res) => {
     res.status(404).send('Not Found');
 });
